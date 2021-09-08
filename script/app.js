d0d3ce29e20291f05be7ce43354eaddd0bcd5437
Simon <simon.coudeville@howest.be>
2021-09-08 12:14:51 +0200

    Muchos updatos

--- script/app.js
+++ script/app.js
@@ -1,15 +1,18 @@
 var btnPause = document.querySelector('.js-pause');
+var btnFast = document.querySelector('.js-fast');
+var btnSlow = document.querySelector('.js-slow');
+var btnNormal = document.querySelector('.js-normal');
 
 btnPause.onclick = function () {
-hoverMaster.paused(!hoverMaster.paused());
+    hoverMaster.paused(!hoverMaster.paused());
     btnPause.classList.toggle('is-paused');
 }
 
-document.querySelector('.js-fast').onclick = () => hoverMaster.timeScale(3);
-document.querySelector('.js-normal').onclick = () => hoverMaster.timeScale(1.5);
-document.querySelector('.js-slow').onclick = () => hoverMaster.timeScale(.75);
+btnFast.onclick = () => hoverMaster.timeScale(3);
+btnNormal.onclick = () => hoverMaster.timeScale(1.5);
+btnSlow.onclick = () => hoverMaster.timeScale(.75);
 
-let hoverMaster = gsap.timeline({repeat: -1 })
+let hoverMaster = gsap.timeline({ repeat: -1 })
     .timeScale(1.5)
     .add(hoverRobot())
     .add(hoverShadow(), '-=2')
@@ -19,18 +22,18 @@ let hoverMaster = gsap.timeline({repeat: -1 })
     .add(hoverArmRight(), '-=2');
 
 function hoverRobot() {
-let tl = gsap.timeline();
+    let tl = gsap.timeline();
     tl
         .fromTo('#Robot', {
-y: 2.5
+            y: 2.5
         },
             {
-y: -5,
+                y: -5,
                 duration: .75,
                 ease: "sine.inOut"
             })
         .to('#Robot', {
-y: 2.5,
+            y: 2.5,
             duration: 1.25,
             ease: "sine.inOut"
         });
@@ -38,21 +41,18 @@ y: 2.5,
 }
 
 function hoverShadow() {
-let tl = gsap.timeline();
+    let tl = gsap.timeline();
     tl
         .set('#Shadow', {
-transformOrigin: "50% 100%",
-            opacity: .125
+            transformOrigin: "50% 100%"
         })
         .to('#Shadow', {
-scale: .75,
-            opacity: .1,
+            scale: .75,
             duration: .75,
             ease: "sine.inOut"
         })
         .to('#Shadow', {
-scale: 1,
-            opacity: .125,
+            scale: 1,
             duration: 1.25,
             ease: "sine.inOut"
         });
@@ -60,18 +60,18 @@ scale: 1,
 }
 
 function hoverFire() {
-let tl = gsap.timeline();
+    let tl = gsap.timeline();
     tl
         .set('#Fire', {
-transformOrigin: "50% 0%"
+            transformOrigin: "50% 0%"
         })
         .to('#Fire', {
-scale: 1.125,
+            scale: 1.125,
             duration: .5,
             ease: "sine.out"
         })
         .to('#Fire', {
-scale: 1,
+            scale: 1,
             duration: 1.25,
             ease: "sine.out"
         });
@@ -79,35 +79,34 @@ scale: 1,
 }
 
 function hoverHead() {
-let tl = gsap.timeline();
+    let tl = gsap.timeline();
     tl
         .to('#Head', {
-y: 1,
+            y: 1,
             duration: .5,
             ease: "Power2.out"
         })
         .to('#Head', {
-y: 0,
+            y: 0,
             duration: .5,
-            // ease: "back.out(4)",
             ease: "sine.out"
         });
     return tl;
 }
 
 function hoverArmLeft() {
-let tl = gsap.timeline();
+    let tl = gsap.timeline();
     tl
         .set('#Arm-left', {
-transformOrigin: "100% 0%"
+            transformOrigin: "100% 0%"
         })
         .to('#Arm-left', {
-rotate: -4,
+            rotate: -4,
             duration: .5,
             ease: "Power2.out"
         })
         .to('#Arm-left', {
-rotate: 0,
+            rotate: 0,
             duration: .75,
             ease: "sine.out"
         })
@@ -115,18 +114,18 @@ rotate: 0,
 }
 
 function hoverArmRight() {
-let tl = gsap.timeline();
+    let tl = gsap.timeline();
     tl
         .set('#Arm-right', {
-transformOrigin: "0% 0%"
+            transformOrigin: "0% 0%"
         })
         .to('#Arm-right', {
-rotate: 4,
+            rotate: 4,
             duration: .5,
             ease: "Power2.out"
         })
         .to('#Arm-right', {
-rotate: 0,
+            rotate: 0,
             duration: .75,
             ease: "sine.out"
         })
